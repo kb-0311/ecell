7b0e14cfb67cd9274bbfad3b11ef66da55d8eab0
Kanishka <kanishka.bansode@gmail.com>
2022-02-13 01:53:12 +0530

    new Contact US From in cloud firestore

--- src/components/Contactus/contact.js
+++ src/components/Contactus/contact.js
@@ -5,6 +5,24 @@ import axios from "axios"
 import { baseUrl } from "../../baseUrl"
 import 'aos/dist/aos.css';
 import './contact.css';
+import { initializeApp } from "firebase/app";
+import { getFirestore ,doc,setDoc } from "firebase/firestore";
+import dotenv from "dotenv";
+dotenv.config();
+
+const firebaseConfig = {
+  apiKey:  process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId:  process.env.FIREBASE_PROJECT_ID,
+  storageBucket:  process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId:  process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId:  process.env.FIREBASE_APPID,
+  measurementId: process.env.FIREBASE_MEASUREMENTID
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+//Initialize Firestore
+const db = getFirestore(app);
 
 const Contact = () => {
     const [name, setname] = useState("")
@@ -33,6 +51,30 @@ const Contact = () => {
       seteamil("");
       setsubject("");
     }
+    const formSubmitFirebase= e =>{
+            
+        
+        const contactForm = document.querySelector('.addForm')
+
+        e.preventDefault()
+        var ids=contactForm.email.value;
+          //Setting the id key for Document as email 
+        setDoc(doc(db,"books",ids), {
+          Name: contactForm.name.value,
+          Email: contactForm.email.value,
+          Subject: contactForm.subject.value,
+          Message: contactForm.message.value
+
+        })
+        .then(() => {
+          alert("form submitted Succesfully");
+            //resetting form as a confirmation for submission 
+          contactForm.reset()
+        })
+
+    }
+    
+
     return (
       <div className="mainb">
         <h1 style={{ color: "#001427" }} data-aos="fade-up">Get in</h1>
@@ -47,23 +89,26 @@ const Contact = () => {
           Leave a message to contact our team
         </p>
         <Container className="form_con" data-aos="fade-up">
-          <form>
+          <form class="addForm">
             <div className="col-lg-6" style={{ display: "inline-block" }}>
               <fieldset>
                 <input
-                  className="input_con"
+                  className="input_con" 
+                  name="name"
                   placeholder="Name"
                   onChange={e => setname(e.target.value)}
                 ></input>
                 <br />
                 <input
                   className="input_con"
-                  placeholder="Email"
+                  name="email"
+                  placeholder="Email" 
                   onChange={e => seteamil(e.target.value)}
                 ></input>
                 <br />
                 <input
                   className="input_con"
+                  name="subject"
                   placeholder="Subject"
                   onChange={e => setsubject(e.target.value)}
                 ></input>
@@ -74,12 +119,13 @@ const Contact = () => {
                 <textarea
                   className="textarea_con"
                   placeholder="Message"
+                  name="message"
                   onChange={e => setmessage(e.target.value)}
                 ></textarea>
               </fieldset>
             </div>
           </form>
-          <button onClick={formsubmit}>Send Message</button>
+          <button onClick={formSubmitFirebase ,formsubmit }>Send Message</button>
         </Container>
         <br></br>
       </div>
